997ba856559f9557b6b43310e2ebef4441f42c75
tzaumiaan <tzaumiaan@gmail.com>
2018-04-22 16:53:39 +0200

    finish rodrigues formula

--- ex1_eigen/src/main.cpp
+++ ex1_eigen/src/main.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
-#include <Eigen/Dense>
-using Eigen::MatrixXd;
+using namespace std;
+
+#include <Eigen/Geometry>
+using namespace Eigen;
+
+Matrix3d skew_sym_matrix(Vector3d vec)
+{
+  Matrix3d m;
+  m << 0, -1*vec(2), vec(1), vec(2), 0, -1*vec(0), -1*vec(1), vec(0), 0;
+  return m;
+}
+
+Matrix3d rotation_matrix_library(double angle, Vector3d axis)
+{
+  AngleAxisd v(angle, axis);
+  return v.matrix();
+}
+
+Matrix3d rotation_matrix_rodrigues(double angle, Vector3d axis)
+{
+  double c = cos(angle);
+  double s = sin(angle);
+  Matrix3d m = c * Matrix3d::Identity();
+  m += (1-c) * (axis * axis.transpose());
+  m += s * skew_sym_matrix(axis); 
+  return m;
+}
 
 int main()
 {
-  MatrixXd m(2,2);
-  m(0,0) = 3;
-  m(1,0) = 2.5;
-  m(0,1) = -1;
-  m(1,1) = m(1,0) + m(0,1);
-  std::cout << m << std::endl;
+  double angle;
+  Vector3d axis;
+  cout.precision(3);
+  
+  angle = M_PI/6;
+  axis << 1, -2, 1;
+  
+  cout << "Given rotation vector with angle = " << angle;
+  cout << " and rotation axis = (" << axis.transpose() << ")^T" << endl;
+  cout << "Rotation matrix of Rodrigues' =" << endl;
+  cout << rotation_matrix_rodrigues(angle, axis) << endl;
+  cout << "Rotation matrix from library =" << endl;
+  cout << rotation_matrix_library(angle, axis) << endl;
+
 }
